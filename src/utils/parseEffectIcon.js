240a98df7439aa9d725055cb08962f4463a39f1e
Victor <visama396@gmail.com>
2024-03-11 20:45:54 +0100

    updates

--- src/utils/parseEffectIcon.js
+++ src/utils/parseEffectIcon.js
@@ -1,12 +1,56 @@
-"https://tmktahu.github.io/WakfuAssets/statistics/distance_mastery.png"
+
 
 const STATISTICS = [
-  
+    {
+        "actionId": 20,
+        "name": "health_points"
+    },
+    {
+        "actionId": 21,
+        "name": "health_points"
+    },
+    {
+        "actionId": 31,
+        "name": "action_points"
+    },
+    {
+        "actionId": 41,
+        "name": "movement_points"
+    },
+    {
+        "actionId": 191,
+        "name": "wakfu_points"
+    },
+    {
+        "actionId": 175,
+        "name": "dodge"
+    },
+    {
+        "actionId": 173,
+        "name": "lock"
+    },
+    {
+        "actionId": 875,
+        "name": "block"
+    },
+    {
+        "actionId": 1052,
+        "name": "melee_mastery"
+    },
+    {
+        "actionId": 1053,
+        "name": "distance_mastery"
+    }
 ]
 
 /**
  * 
  */
 export function parseEffectIcon(actionId) {
-
+    const statName = STATISTICS.find(stat => stat.actionId === actionId)
+    if (statName) {
+        return `https://tmktahu.github.io/WakfuAssets/statistics/${statName.name}.png`
+    } else {
+        return undefined
+    }
 }
