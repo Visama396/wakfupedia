5f4749612aea31cc45c6d79621ef9a368308841a
Victor <visama396@gmail.com>
2024-04-26 10:45:43 +0200

    Update Item.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import netlify from '@astrojs/netlify';
 
-import db from "@astrojs/db";
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind()],
   output: "static",
-  adapter: netlify()
+  adapter: netlify(),
 });
