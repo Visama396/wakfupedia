5ca52610cea3af6934addf5ffe09ee94c0869cb0
Victor <visama396@gmail.com>
2024-09-12 16:13:31 +0200

    Changing Wakpedia output system

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify'
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
+  output: "server",
   adapter: netlify(),
 });
