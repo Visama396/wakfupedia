a31f50757d386f82b58f966486d6b65adebc1257
Victor <visama396@gmail.com>
2025-01-28 12:42:32 +0100

    Added flowbite

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
+  output: "server",
   adapter: netlify(),
 });
