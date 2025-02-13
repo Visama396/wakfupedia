0bc803be18eefdc230006abfae429765d3fda712
Victor <visama396@gmail.com>
2025-02-13 11:21:10 +0100

    Updated favicon
    added new meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://wakpedia.visamalog.com',
+  integrations: [tailwind(), sitemap()],
   adapter: netlify(),
 });
