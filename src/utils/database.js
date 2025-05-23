f8ae19fd136125189fcfec87deb7f6648215ab1d
Victor <visama396@gmail.com>
2025-05-23 23:23:21 +0200

    Added create table script

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,11 +1,16 @@
 import pg from 'pg' 
+import fs from 'fs'
 
 const client = new pg.Client({
     user: import.meta.env.DB_USER,
     host: import.meta.env.DB_HOST,
     database: import.meta.env.DB_DATABASE,
     password: import.meta.env.DB_PASSWORD,
-    port: import.meta.env.DB_PORT
+    port: import.meta.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync('./src/utils/ca.pem').toString()
+    }
 })
 
 client.connect()
